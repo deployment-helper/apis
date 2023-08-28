b9edd0b5063187da162d6c6bc1cb47a22f6eca62
Vinay Kumar <vinaymavi@gmail.com>
2023-08-27 22:41:14 -0400

    Services moved to app level

--- app-management/src/app.module.ts
+++ app-management/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
-import { AppService } from './services/app.service';
+import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { YoutubeModule } from './youtube/youtube.module';
 
