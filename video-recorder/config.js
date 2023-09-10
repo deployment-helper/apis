286723ca42294bcd8cdf65b291072eee0c0cc323
Vinay Kumar <vinaymavi@gmail.com>
2023-09-10 00:01:00 -0400

    recorder config

--- video-recorder/config.js
+++ video-recorder/config.js
@@ -3,20 +3,20 @@ import { config } from "dotenv";
 config();
 
 export const RecorderConfig = {
-    followNewTab: true,
-    fps: 25,    
-    videoFrame: {
-      width: 1024,
-      height: 768,
-    },
-    videoCrf: 18,
-    videoCodec: 'libx264',
-    videoPreset: 'ultrafast',
-    videoBitrate: 1000,
-    autopad: {
-      color: 'black' | '#35A5FF',
-    },
-    aspectRatio: '16:9',
-  };
+  followNewTab: true,
+  fps: 25,
+  videoFrame: {
+    width: 1024,
+    height: 768,
+  },
+  videoCrf: 18,
+  videoCodec: "libx264",
+  videoPreset: "ultrafast",
+  videoBitrate: 1000,
+  autopad: {
+    color: "black" | "#35A5FF",
+  },
+  aspectRatio: "16:9",
+};
 
 export const VIDEO_LOCATION = process.env.VIDEO_LOCATION;
