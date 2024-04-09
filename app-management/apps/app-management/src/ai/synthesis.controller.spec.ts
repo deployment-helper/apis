5c20c4aac5cda0acc54a7c6969e1cdcb5fd80ccd
vinaymavi <vinaymavi@gmail.com>
2024-04-08 22:15:31 -0400

    Unit tests fixes.

--- app-management/apps/app-management/src/ai/synthesis.controller.spec.ts
+++ app-management/apps/app-management/src/ai/synthesis.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+
 import { SynthesisController } from './synthesis.controller';
+import { AwsModule } from '@apps/app-management/aws/aws.module';
+import { GcpModule } from '@app/shared/gcp/gcp.module';
 
 describe('SynthesisController', () => {
   let controller: SynthesisController;
@@ -7,6 +11,7 @@ describe('SynthesisController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SynthesisController],
+      imports: [ConfigModule.forRoot({ isGlobal: true }), AwsModule, GcpModule],
     }).compile();
 
     controller = module.get<SynthesisController>(SynthesisController);
--- app-management/apps/app-management/src/video/project.controller.spec.ts
+++ app-management/apps/app-management/src/video/project.controller.spec.ts
@@ -1,5 +1,9 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { ProjectController } from './project.controller';
+import { AwsModule } from '@apps/app-management/aws/aws.module';
+import { GcpModule } from '@app/shared/gcp/gcp.module';
 
 describe('ProjectController', () => {
   let controller: ProjectController;
@@ -7,6 +11,7 @@ describe('ProjectController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProjectController],
+      imports: [ConfigModule.forRoot({ isGlobal: true }), AwsModule, GcpModule],
     }).compile();
 
     controller = module.get<ProjectController>(ProjectController);
