c22c1efe23a73d29a69521e9415dd363a6012b1a
vinaymavi <vinaymavi@gmail.com>
2024-04-18 19:13:36 -0400

    translate.controller.spec.ts unit tests added.

--- app-management/apps/app-management/src/ai/translate.controller.spec.ts
+++ app-management/apps/app-management/src/ai/translate.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TranslateController } from './translate.controller';
+import { ConfigModule } from '@nestjs/config';
+import { GcpModule } from '@app/shared/gcp/gcp.module';
 
 describe('TranslateController', () => {
   let controller: TranslateController;
@@ -7,6 +9,7 @@ describe('TranslateController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TranslateController],
+      imports: [ConfigModule.forRoot({ isGlobal: true }), GcpModule],
     }).compile();
 
     controller = module.get<TranslateController>(TranslateController);
