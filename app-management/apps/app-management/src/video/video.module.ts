f46a89d3e22cd83a07c0183bc92cf98675da099c
vinaymavi <vinaymavi@gmail.com>
2024-10-11 10:04:01 +0530

    Delete S3 assets when deleting the video.

--- app-management/apps/app-management/src/video/video.module.ts
+++ app-management/apps/app-management/src/video/video.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { VideoController } from './video.controller';
 import { GcpModule } from '@app/shared/gcp/gcp.module';
 import { ProjectController } from './project.controller';
+import { SharedService } from '@app/shared';
+import { AwsModule } from '@app/shared/aws/aws.module';
 
 @Module({
-  imports: [GcpModule],
+  imports: [GcpModule, AwsModule],
+  providers: [SharedService],
   controllers: [VideoController, ProjectController],
 })
 export class VideoModule {}
