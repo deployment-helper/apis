002170d2fed7ff2669432ed8cc385c5266044205
vinaymavi <vinaymavi@gmail.com>
2025-01-23 22:57:37 +0530

    synthesis.service.spec.ts unit tests fixes.

--- app-management/libs/shared/src/gcp/synthesis.service.spec.ts
+++ app-management/libs/shared/src/gcp/synthesis.service.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SynthesisService } from './synthesis.service';
+import { ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { S3Service } from '@app/shared/aws/s3.service';
+import { FsService } from '@app/shared/fs/fs.service';
+import { FontsService } from '@app/shared/fonts.service';
 
 describe('SynthesisService', () => {
   let service: SynthesisService;
 
   beforeEach(async () => {
+    const configServiceMock = {
+      getOrThrow: jest.fn(), // Mock the get method
+    } as unknown as ConfigService;
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SynthesisService],
+      providers: [
+        SynthesisService,
+        FsService,
+        FontsService,
+        S3Service,
+        {
+          provide: ConfigService,
+          useValue: configServiceMock,
+        },
+      ],
+      imports: [HttpModule],
     }).compile();
 
     service = module.get<SynthesisService>(SynthesisService);
